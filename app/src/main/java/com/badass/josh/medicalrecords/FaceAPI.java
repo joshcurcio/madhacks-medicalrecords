b19d10ce17457d941c7870c2e12ec1bf31cb1dc6
Josh Curcio <joshua.curcio@cuw.edu>
2017-10-07 15:06:00 -0500

    add person class

--- app/src/main/java/com/badass/josh/medicalrecords/FaceAPI.java
+++ app/src/main/java/com/badass/josh/medicalrecords/FaceAPI.java
@@ -10,6 +10,8 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import java.net.URI;
+import org.apache.http.client.methods.HttpPut;
 
 import java.net.URI;
 
@@ -22,8 +24,47 @@ public class FaceAPI {
     private static final String URI_BASE = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
 
 
-    public void createPersonGroup(){
+    public void createPersonGroup(Person p){
+        HttpClient httpClient = new DefaultHttpClient();
 
+        try
+        {
+            // The valid characters for the ID below include numbers, English letters in lower case, '-', and '_'.
+            // The maximum length of the personGroupId is 64.
+            String personGroupId = "example-group-00";
+
+            // NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
+            //   For example, if you obtained your subscription keys from westus, replace "westcentralus" in the
+            //   URL below with "westus".
+            URIBuilder builder = new URIBuilder("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/persongroups/" +
+                    personGroupId);
+
+            URI uri = builder.build();
+            HttpPut request = new HttpPut(uri);
+
+            // Request headers. Replace the example key with your valid subscription key.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", "13hc77781f7e4b19b5fcdd72a8df7156");
+
+            // Request body. The name field is the display name you want for the group (must be under 128 characters).
+            // The size limit for what you want to include in the userData field is 16KB.
+            String body = "{ \"name\":\"My Group\",\"userData\":\"User-provided data attached to the person group.\" }";
+
+            StringEntity reqEntity = new StringEntity(body);
+            request.setEntity(reqEntity);
+
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null)
+            {
+                System.out.println(EntityUtils.toString(entity));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void retrieveUserIdFromPicture(){
---
+++ app/src/main/java/com/badass/josh/medicalrecords/Person.java
@@ -0,0 +1,8 @@
+package com.badass.josh.medicalrecords;
+
+/**
+ * Created by Josh on 10/7/2017.
+ */
+
+class Person {
+}
