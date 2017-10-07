64791318a672ad1edeb49fc29ce81424cdf9aea4
timrwild <timrwild@gmail.com>
2017-10-07 14:31:27 -0500

    initialize database

--- app/src/main/java/com/badass/josh/medicalrecords/MainActivity.java
+++ app/src/main/java/com/badass/josh/medicalrecords/MainActivity.java
@@ -8,9 +8,33 @@ public class MainActivity extends AppCompatActivity {
     public static DatabaseHelper bigDatabase;
     public static SQLiteDatabase actualDatabase;
     public static DatabaseInfo maybeDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        bigDatabase = DatabaseHelper.getInstance(this);
+
+        openDB();
+
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        closeDB();
+    }
+
+    private void openDB()
+    {
+        maybeDatabase = new DatabaseInfo();
+        maybeDatabase.open();
+    }
+
+    private void closeDB()
+    {
+        maybeDatabase.close();
+    }
+
 }
