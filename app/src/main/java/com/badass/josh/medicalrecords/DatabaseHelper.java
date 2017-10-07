92c92041ba05a422e2a2593631c1c556206542bf
Josh Curcio <joshua.curcio@cuw.edu>
2017-10-07 13:44:11 -0500

    add database stuff

---
+++ app/src/main/java/com/badass/josh/medicalrecords/DatabaseHelper.java
@@ -0,0 +1,68 @@
+package com.badass.josh.medicalrecords;
+
+/**
+ * Created by Josh on 10/7/2017.
+ */
+
+    import android.content.ContentValues;
+    import android.content.Context;
+    import android.database.sqlite.SQLiteDatabase;
+    import android.database.sqlite.SQLiteOpenHelper;
+    import android.util.Log;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper
+{
+
+    /*
+     * This method is the one that should be called to create the DB. It first checks to see if the database has been
+     * created, and if it hasn't, it gets the app context and creates the DB there instead of in only one context.
+     * If not for this, we would have to recreate the DB every time we go to a new screen.
+     */
+    private static DatabaseHelper savedInstance;
+
+    public static synchronized DatabaseHelper getInstance(Context context) {
+        if (savedInstance == null) {
+            savedInstance = new DatabaseHelper(context.getApplicationContext());
+        }
+        return savedInstance;
+    }
+
+    private DatabaseHelper(Context context) {
+        super(context, DatabaseInfo.DATABASE_NAME, null, DatabaseInfo.DATABASE_VERSION);
+        Log.d("db", "database created");
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DatabaseInfo.DATABASE_CREATE_CARS_TABLE);
+        db.execSQL(DatabaseInfo.DATABASE_CREATE_RECORDS_TABLE);
+        db.execSQL(DatabaseInfo.DATABASE_CREATE_GAS_TABLE);
+        db.execSQL(DatabaseInfo.DATABASE_CREATE_MAINTENANCE_TABLE);
+        db.execSQL(DatabaseInfo.DATABASE_CREATE_GAS_VIEW);
+        db.execSQL(DatabaseInfo.DATABASE_CREATE_MAINTENANCE_VIEW);
+        Log.d("db", "created tables");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        for (int i = 0; i < DatabaseInfo.DATABASE_TABLE_LIST.length; i++) {
+            db.execSQL("DROP TABLE IF EXISTS " + DatabaseInfo.DATABASE_TABLE_LIST[i]);
+        }
+        onCreate(db);
+    }
+
+    public void addRow(DatabaseHelper dh)
+    {
+        SQLiteDatabase db = dh.getWritableDatabase();
+
+        Log.d("this", "started adding the row");
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(DatabaseInfo.DATABASE_TABLE_CARS_MAKE_NAME, "Geo");
+        initialValues.put(DatabaseInfo.DATABASE_TABLE_CARS_MODEL_NAME, "Prizm");
+        initialValues.put(DatabaseInfo.DATABASE_TABLE_CARS_YEAR_NAME, "1995");
+        long k = db.insert(DatabaseInfo.DATABASE_TABLE_CARS, null, initialValues);
+        Log.d("this", "finished adding the row");
+    }
+}
---
+++ app/src/main/java/com/badass/josh/medicalrecords/DatabaseInfo.java
@@ -0,0 +1,117 @@
+package com.badass.josh.medicalrecords;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.util.Log;
+
+import static com.badass.josh.medicalrecords.MainActivity.actualDatabase;
+import static com.badass.josh.medicalrecords.MainActivity.bigDatabase;
+
+/**
+ * Created by timwildauer on 10/7/17.
+ */
+
+public class DatabaseInfo
+{
+    public static final String DATABASE_NAME = "save_starving_kids";
+
+    public static final String DATABASE_TABLE_PATIENTS = "patients";
+    public static final String DATABASE_TABLE_PATIENTS_ID_NAME = "idPatients";
+    public static final int DATABASE_TABLE_PATIENTS_ID_NUM = 0;
+    public static final String DATABASE_TABLE_PATIENTS_NAME_NAME = "name";
+    public static final int DATABASE_TABLE_PATIENTS_NAME_NUM = 1;
+    public static final String DATABASE_TABLE_PATIENTS_DOB_NAME = "DOB";
+    public static final int DATABASE_TABLE_PATIENTS_DOB_NUM = 2;
+    public static final String DATABASE_TABLE_PATIENTS_LOCATION_NAME = "location";
+    public static final int DATABASE_TABLE_PATIENTS_LOCATION_NUM = 3;
+    public static final String[] DATABASE_PATIENTS_NAMES = new String[] {DATABASE_TABLE_PATIENTS_ID_NAME, DATABASE_TABLE_PATIENTS_NAME_NAME, DATABASE_TABLE_PATIENTS_DOB_NAME, DATABASE_TABLE_PATIENTS_LOCATION_NAME};
+
+    public static final String DATABASE_CREATE_PEOPLE_TABLE =
+            "CREATE TABLE IF NOT EXISTS " + DATABASE_TABLE_PATIENTS
+                    + " (" + DATABASE_TABLE_PATIENTS_ID_NAME + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + DATABASE_TABLE_PATIENTS_NAME_NAME + " TEXT NOT NULL, "
+                    + DATABASE_TABLE_PATIENTS_DOB_NAME + " TEXT NOT NULL, "
+                    + DATABASE_TABLE_PATIENTS_LOCATION_NAME + " INTEGER NULL, "
+                    + ");";
+
+    public static final String DATABASE_TABLE_RECORDS = "records";
+    public static final String DATABASE_TABLE_RECORDS_ID_NAME = "idRecords";
+    public static final int DATABASE_TABLE_RECORDS_ID_NUM = 0;
+    public static final String DATABASE_TABLE_RECORDS_PATIENT_ID_NAME = "patientID";
+    public static final int DATABASE_TABLE_RECORDS_PATIENT_ID_NUM = 1;
+    public static final String DATABASE_TABLE_RECORDS_TYPE_NAME = "recordType";
+    public static final int DATABASE_TABLE_RECORDS_TYPE_NUM = 2;
+    public static final String DATABASE_TABLE_RECORDS_DESCRIPTION_NAME = "description";
+    public static final int DATABASE_TABLE_RECORDS_DISTANCE_NUM = 3;
+    public static final String DATABASE_TABLE_RECORDS_DATE_START_NAME = "dateStart";
+    public static final int DATABASE_TABLE_RECORDS_YEAR_NUM = 4;
+    public static final String DATABASE_TABLE_RECORDS_DATE_END_NAME = "dateEnd";
+    public static final int DATABASE_TABLE_RECORDS_MONTH_NUM = 5;
+    public static final String[] DATABASE_RECORDS_NAMES = new String[] {DATABASE_TABLE_RECORDS_ID_NAME, DATABASE_TABLE_RECORDS_PATIENT_ID_NAME, DATABASE_TABLE_RECORDS_TYPE_NAME, DATABASE_TABLE_RECORDS_DESCRIPTION_NAME, DATABASE_TABLE_RECORDS_DATE_START_NAME, DATABASE_TABLE_RECORDS_DATE_END_NAME};
+
+    public static final String DATABASE_CREATE_RECORDS_TABLE =
+            "CREATE TABLE IF NOT EXISTS " + DATABASE_TABLE_RECORDS
+                    + " (" + DATABASE_TABLE_RECORDS_ID_NAME + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + DATABASE_TABLE_RECORDS_PATIENT_ID_NAME + " REFERENCES " + DATABASE_TABLE_PATIENTS + " (" + DATABASE_TABLE_PATIENTS_ID_NAME + "), "
+                    + DATABASE_TABLE_RECORDS_TYPE_NAME + " TEXT NOT NULL, "
+                    + DATABASE_TABLE_RECORDS_DESCRIPTION_NAME + " TEXT NOT NULL"
+                    + DATABASE_TABLE_RECORDS_DATE_START_NAME + " INTEGER NOT NULL, "
+                    + DATABASE_TABLE_RECORDS_DATE_END_NAME + " INTEGER DEFAULT 1, "
+                    + ");";
+
+
+    public static final String DATABASE_VIEW_MAINTENANCE = "maintenance_records";
+
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String[] DATABASE_TABLE_LIST = {DATABASE_TABLE_PATIENTS, DATABASE_TABLE_RECORDS};
+
+
+
+    public long addNewPatient(String name, String DOB, int location)
+    {
+        ContentValues newPatientValues = new ContentValues();
+        newPatientValues.put(DATABASE_TABLE_PATIENTS_NAME_NAME, name);
+        newPatientValues.put(DATABASE_TABLE_PATIENTS_DOB_NAME, DOB);
+        newPatientValues.put(DATABASE_TABLE_PATIENTS_LOCATION_NAME, location);
+
+        return actualDatabase.insert(DATABASE_TABLE_PATIENTS, null, newPatientValues);
+    }
+
+    public long addNewRecord(int id, String type, String description, String startDate, String endDate)
+    {
+        ContentValues newPatientValues = new ContentValues();
+        newPatientValues.put(DATABASE_TABLE_RECORDS_PATIENT_ID_NAME, id);
+        newPatientValues.put(DATABASE_TABLE_RECORDS_TYPE_NAME, type);
+        newPatientValues.put(DATABASE_TABLE_RECORDS_DESCRIPTION_NAME, description);
+        newPatientValues.put(DATABASE_TABLE_RECORDS_DATE_START_NAME, startDate);
+        newPatientValues.put(DATABASE_TABLE_RECORDS_DATE_END_NAME, endDate);
+
+        return actualDatabase.insert(DATABASE_TABLE_RECORDS, null, newPatientValues);
+
+    }
+
+    public Cursor returnAllPatients()
+    {
+        Cursor c = actualDatabase.query(true, DATABASE_TABLE_PATIENTS, DATABASE_PATIENTS_NAMES, null, null, null, null, null, null);
+        return c;
+    }
+
+    public Cursor returnAllRecords(Long car_id)
+    {
+        Log.d("found", "" );
+        Cursor c = actualDatabase.rawQuery("SELECT * FROM " + DATABASE_TABLE_RECORDS + " WHERE " + DATABASE_TABLE_RECORDS_PATIENT_ID_NAME + " = ?", new String[]{"" + car_id});
+        return c;
+    }
+
+
+    public void open()
+    {
+        actualDatabase = bigDatabase.getWritableDatabase();
+    }
+
+    public void close()
+    {
+        bigDatabase.close();
+    }
+}
