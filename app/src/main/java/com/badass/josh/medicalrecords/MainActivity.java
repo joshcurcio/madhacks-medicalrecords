5090936e79976740980fb15a3e334dd505223f87
Josh Curcio <joshua.curcio@cuw.edu>
2017-10-07 14:05:30 -0500

    add database stuff

--- app/src/main/java/com/badass/josh/medicalrecords/MainActivity.java
+++ app/src/main/java/com/badass/josh/medicalrecords/MainActivity.java
@@ -2,9 +2,12 @@ package com.badass.josh.medicalrecords;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.database.sqlite.SQLiteDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static DatabaseHelper bigDatabase;
+    public static SQLiteDatabase actualDatabase;
+    public static DatabaseInfo maybeDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
