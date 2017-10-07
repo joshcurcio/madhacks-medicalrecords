84fd8200f201dc1aadd59d030313bfeb7730f7fe
Josh Curcio <joshua.curcio@cuw.edu>
2017-10-07 18:31:16 -0500

    update camera pic

--- app/src/main/java/com/badass/josh/medicalrecords/WelcomeScreenActivity.java
+++ app/src/main/java/com/badass/josh/medicalrecords/WelcomeScreenActivity.java
@@ -1,17 +1,11 @@
 package com.badass.josh.medicalrecords;
 
-import android.net.Uri;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.util.Log;
 import android.view.View;
 import android.provider.MediaStore;
 import android.graphics.Bitmap;
-
-import java.io.File;
-import java.io.IOException;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
 
@@ -21,33 +15,14 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_screen);
     }
 
-    private static final int REQUEST_TAKE_PHOTO = 0;
-    private static final int REQUEST_SELECT_IMAGE_IN_ALBUM = 1;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    // The URI of photo taken with camera
-    private Uri mUriPhotoTaken;
     public void takePictureButtonPressed(View v) {
         System.out.println("Button press");
-
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            // Save the photo taken to a temporary file.
-            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-            try {
-                File file = File.createTempFile("IMG_", ".jpg", storageDir);
-                mUriPhotoTaken = Uri.fromFile(file);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, mUriPhotoTaken);
-                startActivityForResult(intent, REQUEST_TAKE_PHOTO);
-            } catch (IOException e) {
-                //setInfo(e.getMessage());
-            }
-        }
+        this.dispatchTakePictureIntent();
     }
 
 
-
-
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     // Bring up the camera
     private void dispatchTakePictureIntent() {
@@ -67,4 +42,3 @@ public class WelcomeScreenActivity extends AppCompatActivity {
     }
 
 }
-
