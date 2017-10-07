d94c40f2d8ab1bb689d0e8b998a473d2c9b05635
Josh Curcio <joshua.curcio@cuw.edu>
2017-10-07 18:27:47 -0500

    update camera pic

--- app/src/main/java/com/badass/josh/medicalrecords/WelcomeScreenActivity.java
+++ app/src/main/java/com/badass/josh/medicalrecords/WelcomeScreenActivity.java
@@ -23,6 +23,7 @@ public class WelcomeScreenActivity extends AppCompatActivity {
 
     private static final int REQUEST_TAKE_PHOTO = 0;
     private static final int REQUEST_SELECT_IMAGE_IN_ALBUM = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     // The URI of photo taken with camera
     private Uri mUriPhotoTaken;
@@ -32,13 +33,21 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(intent.resolveActivity(getPackageManager()) != null) {
             // Save the photo taken to a temporary file.
-            dispatchTakePictureIntent();
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            try {
+                File file = File.createTempFile("IMG_", ".jpg", storageDir);
+                mUriPhotoTaken = Uri.fromFile(file);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, mUriPhotoTaken);
+                startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+            } catch (IOException e) {
+                //setInfo(e.getMessage());
+            }
         }
     }
 
 
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
     // Bring up the camera
     private void dispatchTakePictureIntent() {
@@ -48,14 +57,14 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         }
     }
 
-/*    // What do we do when we get the picture back?
+    // What do we do when we get the picture back?
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             System.out.println("requestCode = " + requestCode + "\nresultCode = " + resultCode + "\ndata = " + data);
         }
-    }*/
+    }
 
 }
 
